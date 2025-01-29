2023db9a8bbc5329e304b4ab5af3b1e1123f8619
Rodney Madondo <rodney@finfind.co.za>
2025-01-29 08:43:37 +0200

    updates to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig(
     {  
-        base:'/chatappcssillustrationmaster/',
+        base:'/chat-app-css-illustration/',
         plugins: [ 
                tailwindcss(),
               ],
